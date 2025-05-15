17143f69a25e8564916c71a72131e3e52bec3eb2
Toufiq <mdtoufiqulalam64@gmail.com>
2025-05-15 23:59:41 +0600

    intial commit

---
+++ part4.php
@@ -0,0 +1,25 @@
+<?php
+
+// Inheritance
+
+class Father{
+
+   public function addTwoNumber(){
+        $num1=10;
+        $num2=180;
+        echo $num1+$num2;
+    }
+}
+
+
+
+class Son extends Father{
+
+
+
+}
+
+$SonObj=new Son();
+$SonObj->addTwoNumber();
+
+
