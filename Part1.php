c4d43329791396ac12262c2f30de7f1b8595b4d4
Toufiq <mdtoufiqulalam64@gmail.com>
2025-05-15 21:10:55 +0600

    initial commit

---
+++ Part1.php
@@ -0,0 +1,22 @@
+<?php
+//class
+class person{
+    
+    public function getName1(){
+        echo "Hello I am name\n" ;
+    }
+   
+    public function getName2(){
+        echo "Mohammad Toufiqul Alam";
+    }
+    
+
+}
+//object
+$obj=new person();
+
+$obj->getName1();
+$obj->getName2();
+
+
+?>
---
+++ part2.php
@@ -0,0 +1,27 @@
+<?php 
+
+class car{
+
+public function driving(){
+
+echo "Driving every single day\n";
+
+}
+
+
+public function washing(){
+
+
+echo "Washing Per Weeek";
+
+
+}
+
+
+}
+ $obj= new car();
+ $obj->driving();
+ $obj->washing();
+
+
+?>
